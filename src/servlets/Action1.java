5a3405429beb9b19d9f830133316675f5bb104de
RuicongSu <rs843@njit.edu>
2016-12-01 14:46:24 -0500

    try github

--- src/servlets/Action1.java
+++ src/servlets/Action1.java
@@ -30,6 +30,7 @@ public class Action1 extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("Post get it!");
+		int a = 1;
 	}
 
 }
